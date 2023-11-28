857e2cb8b5514e16023fc4ada3b7a2abd33deece
rafmorei <rafael.mattos09@gmail.com>
2023-11-28 07:34:25 -0300

    cart_details

--- 02.1-backend-java/spring-boot-ecommerce/src/main/java/com/luv2code/commerce/config/MyDataRestConfig.java
+++ 02.1-backend-java/spring-boot-ecommerce/src/main/java/com/luv2code/commerce/config/MyDataRestConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Set;
 
 @Configuration
+@CrossOrigin(origins = "http://localhost:4200")
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     private EntityManager entityManager;
